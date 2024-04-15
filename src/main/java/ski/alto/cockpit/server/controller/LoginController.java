ef4234bd7fa3f2d1e02fa79c2b5c7bd6a84d0b43
Marko Nikolic <marko.nikolic@kentkart.rs>
2024-04-15 22:43:59 +0200

    remove cross-origin

--- src/main/java/ski/alto/cockpit/server/controller/LoginController.java
+++ src/main/java/ski/alto/cockpit/server/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
     UserDAO userDAO;
 
 //    @CrossOrigin(origins = "http://94.127.4.240:4200")	//	SERVER CONFIG
-    @CrossOrigin(origins = {"http://94.127.4.240:4200", "http://localhost:8081", "https://cockpit.alto.ski", "http://65.21.206.110:8081", "https://skiclubgb-cockpit-client-ec942f8fa647.herokuapp.com", "https://skiclub.alto.ski", "https://alto-ski-cockpit-client-393cf5cd662a.herokuapp.com"})
+//    @CrossOrigin(origins = {"http://94.127.4.240:4200", "http://localhost:8081", "https://cockpit.alto.ski", "http://65.21.206.110:8081", "https://skiclubgb-cockpit-client-ec942f8fa647.herokuapp.com", "https://skiclub.alto.ski", "https://alto-ski-cockpit-client-393cf5cd662a.herokuapp.com"})
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request, @RequestParam String ownership) {
 
