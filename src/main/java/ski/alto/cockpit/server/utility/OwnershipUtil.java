e9855f667bc0667e2c7233fa442aabee47f8286e
Marko Nikolic <marko.nikolic@kentkart.rs>
2024-04-19 00:47:37 +0200

    admin user ownership changes

--- src/main/java/ski/alto/cockpit/server/utility/OwnershipUtil.java
+++ src/main/java/ski/alto/cockpit/server/utility/OwnershipUtil.java
@@ -5,19 +5,19 @@ public class OwnershipUtil {
     public static final String SKICLUB_GB = "SKICLUB_GB";
 
     public static String parseOwnership(String ownership) {
-//    	if(ownership == null) {
-//    		return null;
-//    	}else {
-//    		if(ownership.trim().equals("")) {
-//    			return null;
-//    		}else {
-//    			return "SkiClub";
-//    		}
-//    	}
-        if (ownership != null /*&& ownership.equals(SKICLUB_GB)*/) {
-            return "SkiClub";
-        } else {
-            return null;
-        }
+    	if(ownership == null) {
+    		return null;
+    	}else {
+    		if(ownership.trim().equals("")) {
+    			return null;
+    		}else {
+    			return "SkiClub";
+    		}
+    	}
+//        if (ownership != null /*&& ownership.equals(SKICLUB_GB)*/) {
+//            return "SkiClub";
+//        } else {
+//            return null;
+//        }
     }
 }
